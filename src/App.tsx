993d7ef9494f2111f9de25dd04102a1538cc47a1
Hanna Angeria <haan2402@student.miun.se>
2026-02-02 15:17:00 +0100

    gjort en funktion som gör ett get-anrop till api, useEffect vid första rendering, samt lagt in meddelanden för loading och error

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,43 @@ import { useState, useEffect } from "react"
 import type Todo from "./interfaces/Todo"
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
+  const [todos, setTodos] = useState<Todo[]>([]);           //state som lagrar listan med todos
+  const [loading, setLoading] = useState<boolean>(false);   //state för att visa laddningsmeddelanden
+  const [error, setError] = useState<string | null>(null);  //state för felhantering vid API-anrop
+
+  //körs vid första rendering, hämtar todos från API
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  //funktion för att hämta todos från API
+  const getTodos = async () => {
+    try {
+      setLoading(true); //startar indikator för laddning
+      setError(null);   //nollställer tidigare felmeddelanden
+
+      //GET-anrop till backend API
+      const response = await fetch("http://localhost:5000/todos");
+
+      //kontroll om anrop är ok
+      if(response.ok) {
+        const data = await response.json();
+        setTodos(data);
+      }
+
+    } catch(error) {
+      setError("Något gick fel vid hämtning av todos, försök igen senare!")
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <>
     <main>
     <h1>Att göra-lista</h1>
-
+    {error && <p>{error}</p>}
+    {loading && <p>Laddar in data...</p>}
     </main>
     </>
   )
