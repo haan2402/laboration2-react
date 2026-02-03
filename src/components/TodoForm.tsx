3230cdb68c6ee51f1cbfae3d03517ae446b38d35
Hanna Angeria <haan2402@student.miun.se>
2026-02-03 14:02:52 +0100

    lagt till validering av input fälten för titel och beskrivning, lagt till Post anrop för att lägga till ny todo till listan

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -10,24 +10,85 @@ export const TodoForm = ({todoCreated} : {todoCreated : Function}) => {
         status: string
     }
 
-    const [formData, setFormData] = useState<TodoFormData>({title: "", description: "", status: "Ej påbörjad"});  //state som lagrar data från formuläret
-    const status = ["Ej påbörjad", "Pågående", "Avklarad"];                                                       //array med olika alternativ för status
+    //interface för felhantering
+    interface TodoFormError {
+        title?: string,
+        description?: string
+    }
+
+    //state som lagrar data från formuläret, samt array med olika alternativ för status
+    const [formData, setFormData] = useState<TodoFormData>({title: "", description: "", status: "Ej påbörjad"});  
+    const status = ["Ej påbörjad", "Pågående", "Avklarad"];                                                       
+
+    //state för felhantering
+    const [errors, setError] = useState<TodoFormError>({});                                                          
+
+    //funktion för validering av titel och beskrivning
+    const validateForm = ((data: TodoFormData) => {
+        const validateError: TodoFormError = {};
+
+        if(data.title.trim().length < 3) {
+            validateError.title = "Titeln måste vara minst 3 tecken!";
+        }
+
+        if(data.description.trim().length > 200) {
+            validateError.description = "Beskrivning får max vara 200 tecken!";
+        }
+
+        return validateError;
+    })
 
     //funktion som körs när formulär skickas, förhindrar att sidan laddas om
-    const formSubmit = ((event: any) => {
+    const formSubmit = async (event: any) => {
         event.preventDefault();
-    });
 
-    //formulär data skickas till backend API för att skapa en ny todo
+        const validateError = validateForm(formData);
+
+        if(Object.keys(validateError).length > 0) {
+            setError(validateError);
+            return;
+        } else {
+            setError({});
+        }
+
+        //POST-anrop till backend för att skapa en ny todo
+        try {
+            const respone = await fetch("http://localhost:5000/todos/", {
+                method: "POST",
+                headers: {
+                    "Content-type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            });
+
+            if(respone.ok) {
+                //uppdaterar listan med todos
+                todoCreated();
+
+                //rensar formuläret
+                setFormData({title: "", description: "", status: "Ej påbörjad"});
+            }
+        } catch(error) {
+            console.log("Gick inte att skapa en ny todo...", error);
+        }
+    }
+
+    //formulär data skickas till backend API för att skapa en ny todo, valideringmeddelande för namn och beskrivning
   return (
     <form onSubmit={formSubmit}>
         <label htmlFor="title">Titel</label>
         <input type="text" name="title" id="title" value={formData.title} 
         onChange={(event) => setFormData({...formData, title: event.target.value})}/>
+
+        {errors.title && <span>{errors.title}</span>}
+
         <br />
         <label htmlFor="description">Beskrivning</label>
         <input type="text" name="description" id="description" value={formData.description} 
         onChange={(event) => setFormData({...formData, description: event.target.value})} />
+
+        {errors.description && <span>{errors.description}</span>}
+
         <br />
         <label htmlFor="status">Status: </label>
         <select name="status" id="status" value={formData.status} 
